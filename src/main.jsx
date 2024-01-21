eab8a94ce0bda1a444dca86990fbfd7c037b5093
RareMashiro <73781398+RareMashiro@users.noreply.github.com>
2024-01-21 17:41:29 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/no-children-prop */
+import ReactDOM from 'react-dom/client'
+import { restaurants } from '../materials/mock';
+// import React from 'react';
+// import { restaurants } from '../materials/mock';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render();
+
