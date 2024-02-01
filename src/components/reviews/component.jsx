3c5855ceb6b49a736944b71f23466c4ae154c145
RareMashiro <symon.belmont2015@yandex.ru>
2024-02-01 16:33:15 +0300

    ДЗ4 выполнено

--- src/components/reviews/component.jsx
+++ src/components/reviews/component.jsx
@@ -1,11 +1,24 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
 import { Review } from "../review/component"
+import { ReviewForm } from "../reviewForm/component"
+import styles from './styles.module.scss';
 
 export const Reviews = ({reviews}) => {
     return (
-        <ul>
-            {reviews.map(pos => {
-                    return <li className='review-item'><Review review={pos} /></li>
+        <div className={styles.bottom}>
+            <ul>
+                {reviews.map(pos => {
+                    return (
+                        <li className={styles.reviewItem}
+                            ><Review review={pos} />
+                        </li>
+                    )
                 })}
-        </ul>
+                
+            </ul>
+            <ReviewForm />
+        </div>
+        
     )
 }
