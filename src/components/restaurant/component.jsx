de3e5d1266b1b526907ef0375392b79b53d262e3
RareMashiro <symon.belmont2015@yandex.ru>
2024-02-11 19:26:35 +0300

    ДЗ6 добавил redux, задействовал селекторы нормализованных данных в компонентах. Возникла ошибка - ревью из норм. данных в 1 ресторане вызываются дважды

--- src/components/restaurant/component.jsx
+++ src/components/restaurant/component.jsx
@@ -1,18 +1,23 @@
 /* eslint-disable react/jsx-key */
 import { Reviews } from "../reviews/component";
 import { Menu } from "../menu/component";
-//import './styles.scss';
 import { Name } from "../name/component";
+import { useSelector } from "react-redux";
+import { selectRestaurantById } from "../../../redux/entities/restaurant/selectors";
 
 /* eslint-disable react/prop-types */
-export const Restaurant = ({restaurant, key}) => {
+export const Restaurant = ({ id }) => {
+    
+    const restaurant = useSelector((state) => selectRestaurantById(state, id))
+    console.log(restaurant.reviews);
+
     return (
         <div>
-            <Name place={restaurant.name} />
+            <Name place={restaurant} />
             <h3 className='menu-title'><span>Menu</span></h3>
             <Menu menu={restaurant.menu} />
             <h3 className='review-title'><span>Reviews</span></h3>
-            <Reviews reviews={restaurant.reviews} key={key}/>
+            <Reviews reviews={restaurant.reviews} key={id}/>
         </div>
     )
 }
